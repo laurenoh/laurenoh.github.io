0738ccac5053c45d7256987c6efa39b47db14430
Lauren Oh <yjo0101@gmail.com>
2020-02-05 18:14:52 -0800

    fix linking to home page

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 // from work page, clicking on work makes the pg scroll to recent work //
-const workLinks = document.querySelectorAll('a[href*="#work"]')
-let target = document.querySelector(".recent-work-padding")
+
 
 const arrowBox = document.querySelector(".arrow-box");
 const projectContainer = document.querySelector(".project-container");
 
-workLinks.forEach(workLink => {
-  workLink.addEventListener('click', (e)=> {
-    e.preventDefault()
-    console.log("click")
-    let hashval = workLink.getAttribute('href')
-    console.log("hashval", hashval)
-    target.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
+if (window.location.includes("index.html")) {
+  const workLinks = document.querySelectorAll('a[href*="#work"]')
+  const target = document.querySelector(".recent-work-padding")
+  workLinks.forEach(workLink => {
+    workLink.addEventListener('click', (e)=> {
+      e.preventDefault()
+      console.log("click")
+      let hashval = workLink.getAttribute('href')
+      console.log("hashval", hashval)
+      target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+      })
+      // adds to history stack when you go back and forth between windows
+      history.pushState(null,null,hashval)
     })
-    // adds to history stack when you go back and forth between windows
-    history.pushState(null,null,hashval)
   })
-})
+}
+
 
 arrowBox.addEventListener("click", () => {
   projectContainer.scrollIntoView({
