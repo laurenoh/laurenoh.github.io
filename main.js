8a96c685d93527e7c4cb8707a5f1c9bbd0fd7465
Lauren Oh <yjo0101@gmail.com>
2020-02-05 17:39:25 -0800

    about me arrow down

--- main.js
+++ main.js
@@ -25,3 +25,40 @@
       })
     }
   }
+
+
+
+
+
+
+
+
+
+
+  // from work page, clicking on work makes the pg scroll to recent work //
+    const arrowBox = document.querySelector('a[href^="#arrowBox"]')
+    let target = document.querySelector(".plunge-paragraph")
+
+    workLink.addEventListener('click', (e)=> {
+      console.log("click")
+      let hashval = workLink.getAttribute('href')
+
+      target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+      })
+  // adds to history stack when you go back and forth between windows//
+
+      history.pushState(null,null,hashval)
+      e.preventDefault()
+    })
+
+  // from non-work pgs, scrolls down to recent work due to #work from URL//
+    window.onload = () => {
+      if (window.location.hash === "#arrowBox") {
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+      }
+    }
